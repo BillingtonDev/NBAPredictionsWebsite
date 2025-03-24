58a6aae37ae46a766fb665ca0f8cc3a82a6f10d8
BillingtonDev <119828882+BillingtonDev@users.noreply.github.com>
2025-03-24 16:49:33 -0400

    MB - get team players

--- ml/players.py
+++ ml/players.py
@@ -1,13 +1,13 @@
 from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.static import players
+from nba_api.stats.endpoints import commonteamroster
 import pandas as pd
+import time
 
 def calculate_player_rating(stats):
     weights = {'PTS': 1.0, 'REB': 0.8, 'AST': 0.7, 'STL': 1.2, 'BLK': 1.1, 'TOV': -1.0}
-    #rating = stats['PTS'] * weights['PTS'] + stats['REB'] * weights['REB'] + stats['AST'] * weights['AST'] + stats['STL'] * weights['STL'] + stats['BLK'] * weights['BLK'] + stats['TOV'] * weights['TOV']
-    rating = sum(stats[stat] * weight for stat, weight in weights.items())
-    
-    return round(rating, 2)
+    rating = sum(stats[stat] * weight for stat, weight in weights.items() if stat in stats)
+    return (round(rating, 2) * 10)
 
 def get_player_id(player_name):
     players_dict = players.get_players()
@@ -18,7 +18,47 @@ def get_player_id(player_name):
 
 def get_player_stats(player_id):
     career = playercareerstats.PlayerCareerStats(player_id=player_id)
-    df = pd.DataFrame(career.get_data_frames()[0]).iloc[0]
-    return df
+    stats = career.get_dict()
 
-print(calculate_player_rating(get_player_stats(player_id='203999')))
+    if stats['resultSets'][0]['rowSet']:
+        latest_season = stats['resultSets'][0]['rowSet'][-1]
+        headers = stats['resultSets'][0]['headers']
+        return dict(zip(headers, latest_season))
+    return None
+
+def estimate_team_stats():
+    df = pd.DataFrame()
+    team_id = '1610612737'
+    team_players = get_team_players(team_id)
+    print("Loading...")
+    try:
+        for id in team_players['PLAYER_ID']:
+            row = get_player_stats(player_id=id)
+            time.sleep(1)
+            df = pd.concat([df, pd.DataFrame([row])], ignore_index=True)
+            
+            
+        print("Success!")
+        print(df)
+        
+    except Exception as error:
+        print(error)
+    
+def get_team_players(team_id):
+    try:
+        roster = commonteamroster.CommonTeamRoster(team_id=team_id)
+        players_data = roster.get_dict()
+    
+        if 'resultSets' in players_data and players_data['resultSets'][0]['rowSet']:
+            headers = players_data['resultSets'][0]['headers']
+            players = players_data['resultSets'][0]['rowSet']
+            
+            # Convert to DataFrame
+            df = pd.DataFrame(players, columns=headers)
+            return df[['PLAYER_ID', 'PLAYER', 'POSITION', 'HEIGHT', 'WEIGHT']]
+    
+    except Exception as error:
+        print(error)
+        
+# print(calculate_player_rating(get_player_stats(player_id='203999')))
+estimate_team_stats()
