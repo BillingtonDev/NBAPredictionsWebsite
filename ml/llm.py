64344cec43453e331ccbdb9f76a51ff1e9e7be9a
sjsaug <sjsaug@gmail.com>
2025-03-26 12:41:34 -0400

    hardcode matchup data for now

--- ml/llm.py
+++ ml/llm.py
@@ -8,8 +8,9 @@ You are a statistical analyst for the NBA. Based on the following team ratings a
 For example: if the score of the Magic is A and the score of the Bulls is B, and the given team matchup is Magic vs Bulls, use the fractional odds formula to calulate the odds.
 The formula is as follows: `fractional_odds = exp(team_score_difference) / (1 + exp(team_score_difference))` Then multiply by 100 to get a decimal value, and format it as `X:Y`.
 For example: if the output is `2.5`, you can display it as `2.5:1`.
+IMPORTANT: ONLY RETURN THE OUTPUT (ODDS) IE. 2.5:1 (NOTHING ELSE)
 
-The team matchup is: {team_matchup}
+The team matchup is: Bulls vs Raptors
 The team ratings are as follows: {team_ratings}
 """
 def main():
@@ -19,7 +20,7 @@ def main():
     completion = client.chat.completions.create(model="gpt-4o", messages=[{
                 "role": "user",
                 #TODO: need to find a way to get what matchup we want to predict from the frontend. ie. Raptors vs Bulls
-                "content": ChatPromptTemplate.from_template(PROMPT_TEMPLATE).format(team_ratings=ratings, team_matchup=matchup)}])
+                "content": ChatPromptTemplate.from_template(PROMPT_TEMPLATE).format(team_ratings=ratings)}])
     print(completion.choices[0].message.content)
 
 if __name__ == "__main__":
